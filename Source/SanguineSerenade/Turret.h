40eb111673fe1c71ce66ccc637fc84cadc26f4d2
Kristoffer Benjaminsen <33286455+Lillebenn@users.noreply.github.com>
2022-10-29 18:41:03 +0200

    C++ files & base turret class

---
+++ Source/SanguineSerenade/Turret.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Turret.generated.h"
+
+UCLASS()
+class SANGUINESERENADE_API ATurret : public AActor
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		USceneComponent* Root;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		USkeletalMeshComponent* TurretMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	class UCapsuleComponent* Capsule;
+	
+public:	
+	// Sets default values for this actor's properties
+	ATurret();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
